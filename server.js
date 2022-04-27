6032f1f13644703c088c87266c2fdf354a03c06a
majdifkih <majdifkih123@gmail.com>
2022-04-27 02:49:50 +0100

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const req = require('express/lib/request')
 const app = express()
-const port =  5000
+const port =  process.env.PORT || 5000
 const mongoose= require('mongoose')
 const cors = require('cors')
 import dotenv from "dotenv";
