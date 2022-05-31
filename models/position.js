59db943fdde2b129141f93c2b801f35be798363e
majdifkih <majdifkih123@gmail.com>
2022-05-31 20:20:37 +0100

    commit

--- models/position.js
+++ models/position.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose')
 
-
 const positionSchema = new mongoose.Schema({
     id:{
         type:String,
         required:true,
         unique:true
     },
-
       latitude:{
         type:String,
         required:true,
        
     },
-
     longutide:{
         type:String,
         required:true
@@ -27,7 +24,6 @@ const positionSchema = new mongoose.Schema({
         required:true
     }
 });
-
 const Position = mongoose.model('position' ,positionSchema)
 module.exports = Position
  
